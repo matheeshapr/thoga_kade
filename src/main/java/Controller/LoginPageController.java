d3e9c03ce0d47f1198c3be782b22874e9a3285c7
matheeshapr <matheeshap62@gmail.com>
2025-10-09 17:37:31 +0530

    Add dashboard and login page FXML files with corresponding controllers

--- src/main/java/Controller/LoginPageController.java
+++ src/main/java/Controller/LoginPageController.java
@@ -1,4 +1,20 @@
 package Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LoginPageController {
+    Stage stage = new Stage();
+    public void loginonaction(ActionEvent actionEvent) {
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/dashboard_page.fxml"))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        stage.show();
+    }
 }
