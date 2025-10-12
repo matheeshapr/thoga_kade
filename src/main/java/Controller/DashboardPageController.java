0c48fd74faf83e6ae2acc8e56c70e552a7628da8
matheeshapr <matheeshap62@gmail.com>
2025-10-12 23:02:37 +0530

    add loging page image and edite dashboard

--- src/main/java/Controller/DashboardPageController.java
+++ src/main/java/Controller/DashboardPageController.java
@@ -1,4 +1,17 @@
 package Controller;
 
+import javafx.event.ActionEvent;
+
 public class DashboardPageController {
+    public void cusmanageaction(ActionEvent actionEvent) {
+    }
+
+    public void itemmangeaction(ActionEvent actionEvent) {
+    }
+
+    public void supmanageaction(ActionEvent actionEvent) {
+    }
+
+    public void empmanageaction(ActionEvent actionEvent) {
+    }
 }
