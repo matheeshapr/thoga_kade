ae2045b21cdd4d843ac705a1c96e75fedbc43035
matheeshapr <matheeshap62@gmail.com>
2025-10-13 13:36:28 +0530

    edit customer page

--- src/main/java/Controller/CustomerPageController.java
+++ src/main/java/Controller/CustomerPageController.java
@@ -1,4 +1,20 @@
 package Controller;
 
+import javafx.event.ActionEvent;
+
 public class CustomerPageController {
+    public void dashaction(ActionEvent actionEvent) {
+    }
+
+    public void itemaction(ActionEvent actionEvent) {
+    }
+
+    public void suppaction(ActionEvent actionEvent) {
+    }
+
+    public void empaction(ActionEvent actionEvent) {
+    }
+
+    public void logoutaction(ActionEvent actionEvent) {
+    }
 }
