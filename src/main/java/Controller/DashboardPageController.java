7892780e19faff4962f0df32ce2ad2bd9b9a4002
matheeshapr <matheeshap62@gmail.com>
2025-10-13 12:01:56 +0530

    edit dashboard ui

--- src/main/java/Controller/DashboardPageController.java
+++ src/main/java/Controller/DashboardPageController.java
@@ -3,15 +3,22 @@ package Controller;
 import javafx.event.ActionEvent;
 
 public class DashboardPageController {
-    public void cusmanageaction(ActionEvent actionEvent) {
+
+    public void dashaction(ActionEvent actionEvent) {
     }
 
-    public void itemmangeaction(ActionEvent actionEvent) {
+    public void custaction(ActionEvent actionEvent) {
     }
 
-    public void supmanageaction(ActionEvent actionEvent) {
+    public void itemaction(ActionEvent actionEvent) {
     }
 
-    public void empmanageaction(ActionEvent actionEvent) {
+    public void suppaction(ActionEvent actionEvent) {
+    }
+
+    public void empaction(ActionEvent actionEvent) {
+    }
+
+    public void logoutaction(ActionEvent actionEvent) {
     }
 }
