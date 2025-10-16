cdeed3f40b14cf599d55eefb14564fc6458e9913
matheeshapr <matheeshap62@gmail.com>
2025-10-16 22:48:12 +0530

    add image for dashboard page

--- src/main/java/Controller/CustomerPageController.java
+++ src/main/java/Controller/CustomerPageController.java
@@ -1,9 +1,25 @@
 package Controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class CustomerPageController {
+
     public void dashaction(ActionEvent actionEvent) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/view/dashboard_page.fxml"));
+            Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void itemaction(ActionEvent actionEvent) {
@@ -16,5 +32,17 @@ public class CustomerPageController {
     }
 
     public void logoutaction(ActionEvent actionEvent) {
+    }
+
+    public void addaction(ActionEvent actionEvent) {
+    }
+
+    public void viewaction(ActionEvent actionEvent) {
+    }
+
+    public void updateaction(ActionEvent actionEvent) {
+    }
+
+    public void deleteaction(ActionEvent actionEvent) {
     }
 }
--- src/main/java/Controller/DashboardPageController.java
+++ src/main/java/Controller/DashboardPageController.java
@@ -1,14 +1,27 @@
 package Controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class DashboardPageController {
 
-    public void dashaction(ActionEvent actionEvent) {
+    public void custaction(ActionEvent actionEvent) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/view/customer_page.fxml"));
+            Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public void custaction(ActionEvent actionEvent) {
-    }
 
     public void itemaction(ActionEvent actionEvent) {
     }
